32a21da2ac7032ced4fb6fbffdb7ad4d89ce8710
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-07-06 15:22:30 -0400

    Columns Sandbox

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:coffee_card/home.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Home()));
+  // runApp(const MaterialApp(home: Home()));
+  runApp(const MaterialApp(home: Sandbox()));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(width: 100, color: Colors.red, child: Text('one')),
+          Container(width: 200, color: Colors.green, child: Text('two')),
+          Container(width: 300, color: Colors.blue, child: Text('three')),
+        ],
+      ),
+    );
+  }
 }
