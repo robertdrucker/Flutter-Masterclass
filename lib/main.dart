abf0f7d110af507ae8fb61da2e2b4dda6be489a7
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-07-06 13:29:58 -0400

    Containers

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Home(),
+          title: const Text('My Coffee Id'),
           backgroundColor: Colors.brown[700],
           centerTitle: true,
         ),
@@ -19,6 +19,23 @@ class Home extends StatelessWidget {
   const Home({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello, ninjas!!!');
+    return Container(
+      color: Colors.orange,
+      // width: 200,
+      // height: 200,
+      padding: const EdgeInsets.all(20),
+      // margin: const EdgeInsets.only(left: 50),
+      margin: const EdgeInsets.fromLTRB(10, 40, 0, 0),
+      // child must be last
+      child: const Text(
+        'Hello, ninjas',
+        style: TextStyle(
+          fontSize: 18,
+          letterSpacing: 4,
+          decoration: TextDecoration.underline,
+          fontStyle: FontStyle.italic,
+        ),
+      ),
+    );
   }
 }
