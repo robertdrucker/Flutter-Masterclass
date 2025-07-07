829ce44439661bb1cbd140a096aca0bbfe2f53e8
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-07-07 05:24:35 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masterclass/screens/home/home.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Home()));
+}
+
+// sandbox
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: const Text('sandbox'),
+    );
+  }
+}
